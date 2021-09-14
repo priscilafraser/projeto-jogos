3eb8f5c3f5d962f3d386281ec057a94b8d415e21
priscilafraser <priscila.fraser@gmail.com>
2021-09-13 22:33:37 -0400

    importando dados no mongodb

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require("express"); 
+const express = require("express");
 
 const app = express(); 
 
@@ -6,64 +6,6 @@ const porta = 4000;
 
 app.use(express.json()); 
 
-const jogos = [
-  {
-    id: 1,
-    nome: "Mortal Kombat",
-    lancamento: 1992,
-    genero: "luta",
-    img: "https://2.bp.blogspot.com/-IgSNsptdQ9U/WV1QaDG5EhI/AAAAAAAADg4/4QvkM5w1JcgAWCJaW_Xj_P-qUgRT5dLmgCLcBGAs/s1600/Mortal%2BKombat%2B1%2B%2528PT-BR%2529%2B-%2B%255Bsnes-forever.blogspot.com%255D-20170705-174619.png"
-  },
-  {
-    id: 2,
-    nome: "Tekken",
-    lancamento: 1994,
-    genero: "Luta",
-    img: "https://upload.wikimedia.org/wikipedia/pt/thumb/c/ca/Tekken_1_capa.png/420px-Tekken_1_capa.png"
-  },
-  {
-    id: 3,
-    nome: "The Need for Speed",
-    lancamento: 1994,
-    genero: "Corrida",
-    img:  "https://upload.wikimedia.org/wikipedia/pt/8/85/The_NFS_Video_cover.jpg"
-  },
-  {
-    id: 4,
-    nome: "Top Gear",
-    lancamento: 1992,
-    genero: "Corrida",
-    img:  "https://i.ytimg.com/vi/kPL6lo3ITs0/maxresdefault.jpg"
-  },
-  {
-    id: 5,
-    nome: "Super Mario World",
-    lancamento: 1990,
-    genero: "Plataforma",
-    img:  "https://upload.wikimedia.org/wikipedia/pt/thumb/0/06/Super-Mario-World.jpg/420px-Super-Mario-World.jpg"
-  },
-  {
-    id: 6,
-    nome: "Streets of Rage",
-    lancamento: 1991,
-    genero: "Luta",
-    img:  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQc8erPHZKcbenmyIl9_litWIy7b_wVHGMF7w&usqp=CAU"
-  },
-  {
-    id: 7,
-    nome: "Sonic",
-    lancamento: 1991,
-    genero: "Plataforma",
-    img:  "https://i.ytimg.com/vi/J06663Njops/maxresdefault.jpg"
-  },
-  {
-    id: 8,
-    nome: "Gran Turismo",
-    lancamento: 1997,
-    genero: "Simulador de corrida",
-    img:  "https://upload.wikimedia.org/wikipedia/pt/thumb/4/47/Gran_Turismo_1997_capa.png/405px-Gran_Turismo_1997_capa.png"
-  }
-];
 
 
 //CRUD
